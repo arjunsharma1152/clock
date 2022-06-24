b58eb5851452158ca3345d460c42a3c3ce8484ed
arjun sharma <arjunsk923@gmail.com>
2022-06-24 15:05:44 +0530

    changed files

---
+++ script.js
@@ -0,0 +1,31 @@
+var hms=document.getElementById("h_m_s");
+
+function displaytime(){
+	var date= new Date();
+
+	let h=date.getHours();
+	let m=date.getMinutes();
+	let s=date.getSeconds();
+
+	let am_pm="AM";
+
+	if(h>12){
+		am_pm="PM";
+		h=h%12;
+	}
+
+	if(h<10)
+		h="0" + h;
+	if(m<10)
+		m="0" + m;
+	if(s<10)
+		s="0" + s;
+
+		hms.innerHTML = h+":"+m+":"+s+" "+am_pm;
+
+		setTimeout(function(){displaytime()},1000);	
+}
+
+
+
+displaytime();
